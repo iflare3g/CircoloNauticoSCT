78230ab29f7c9273e0e2bfd706eac18ead2ab529
Mirco Grillo <mirco.grillomg@gmail.com>
2018-04-23 13:33:46 +0200

    Creates timeline component instead of write boilerplate code into the screen file

--- components/Timeline/Timeline.js
+++ components/Timeline/Timeline.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
 import Timeline from 'react-native-timeline-listview';
-import styles from './style';
 
 const TimelineComp = (props) => (
-  <View>
-    <Timeline
-      style={ styles.list }
-      data={ props.data }
-      circleSize={ 20 }
-      circleColor='rgb(45,156,219)'
-      lineColor='rgb(45,156,219)'
-      timeContainerStyle={ styles.timeContainerStyle }
-      timeStyle={ styles.timeStyle }
-      descriptionStyle={ styles.descriptionStyle }
-      options={ styles.options }
-      innerCircle={ 'dot' }
-    />
-  </View>
+  <Timeline
+     style={styles.list}
+     data={props.data}
+     circleSize={20}
+     circleColor='#1a0dab'
+     lineColor='#1a0dab'
+     timeContainerStyle={{minWidth:52}}
+     timeStyle={{textAlign: 'center', backgroundColor:'#ff9797', color:'white', padding:5, borderRadius:13, marginBottom: 80}}
+     descriptionStyle={{color:'gray'}}
+     options={{
+       style:{paddingTop:60}
+     }}
+     innerCircle={'dot'}
+     detailContainerStyle={{ backgroundColor: '#d9d9d9', borderRadius: 13}}
+     titleStyle={{ fontSize: 18, textAlign: 'center'}}
+     columnFormat='single-column-right'
+     />
 );
 
 export default TimelineComp;
